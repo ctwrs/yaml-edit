ba3967ddcf575389aa37d8f1effaaa24183cfd9d
ctwrs <>
2022-11-24 00:33:51 +0100

    - validation and conflict checking for adding tags and items - DONE - fixed header and 1st row with a solid/semi-transparent background - DONE - better input boxes - kinda DONE - full-width Item mode (will work as a separate table row, easy to do) - DONE
    - selecting checkboxes and mousing away too soon may prevent saving of the checkbox state if it falls under the 6 visible checkboxes limit - FIXED
    - selected checkboxes don't rearange themselves to the top - FIXED

--- islands/Main.tsx
+++ islands/Main.tsx
@@ -1,12 +1,17 @@
 import { lodash as _ } from "lodash";
 import { parse, stringify } from "yaml";
 
-import { useMemo } from "preact/hooks";
-import { Signal, signal, useSignal } from "@preact/signals";
+import { useEffect, useMemo } from "preact/hooks";
+import { Signal, signal, useComputed, useSignal } from "@preact/signals";
 
 import { yamlFile, yamlTags } from "../data/TestData.ts";
 
 export const error = signal<Error | null>(null);
+
+type Config = {
+  item_in_separate_row: boolean;
+};
+const config = signal<Config>({ item_in_separate_row: false });
 
 type Tags = Record<string, string[]>;
 const parsedTags = signal<Tags>({});
@@ -23,7 +28,7 @@ const parseTags = (tags: string) => {
   return parsed;
 };
 
-type File = Record<string, {Tags: string[]}>;
+type File = Record<string, { Tags: string[] }>;
 const parsedFile = signal<File>({});
 
 const parseFile = (file: string) => {
@@ -40,24 +45,20 @@ const parseFile = (file: string) => {
 };
 
 const CheckboxTag = function CheckboxTag(
-  props: { tags: string[]; tag: string },
+  props: { toggleTag: (tag: string) => void; tags: Signal<string[]>; tag: string },
 ) {
-  const tagIndex = props.tags.indexOf(props.tag);
+  const uuid = useMemo(() => crypto.randomUUID(), []);
+  const tagIndex = props.tags.value.indexOf(props.tag);
   return (
     <div class="flex items-center">
       <input
         type="checkbox"
+        id={uuid}
         class="h-4 w-4 text-indigo-600 transition duration-150 ease-in-out"
         checked={tagIndex > -1}
-        onChange={() => {
-          if (tagIndex > -1) {
-            props.tags.splice(tagIndex, 1);
-          } else {
-            props.tags.push(props.tag);
-          }
-        }}
+        onChange={() => props.toggleTag(props.tag)}
       />
-      <label class="ml-2 block text-sm leading-5 text-gray-900">
+      <label for={uuid} class="ml-2 block text-sm leading-5 text-gray-900">
         {props.tag}
       </label>
     </div>
@@ -73,20 +74,38 @@ const partition = (arr: string[], predicate: (item: string) => boolean) => {
 };
 
 const TagCategory = function TagCategory(
-  props: { tags: string[]; category: string[]; categoryName: string },
+  props: { tags: Signal<string[]>; category: string[]; categoryName: string },
 ) {
   const showList = useSignal(false);
-  const [checked, unchecked] = useMemo(() => partition(props.category, (tag) => props.tags.includes(tag)), [props.tags, props.category]);
+
+  const [checked, unchecked] = useMemo(
+    () => partition(props.category, (tag) => props.tags.value.includes(tag)),
+    [props.tags.value, props.category],
+  );
+  const toggleTag = (tag: string) => {
+    if (props.tags.value.includes(tag)) {
+      props.tags.value = props.tags.value.filter((t) => t !== tag);
+    } else {
+      props.tags.value = [...props.tags.value, tag];
+    }
+  };
   return (
-    <td class="text-xs px-1 text-gray-300"
-    onMouseEnter={() => showList.value = true}
-      onMouseLeave={() => showList.value = false}>
+    <td
+      class="text-xs px-1 text-gray-300"
+      onMouseEnter={() => showList.value = true}
+      onMouseLeave={() => {
+        showList.value = false
+
+      }}
+    >
       <ul>
         {props.category &&
           // props.category.filter((tag, i) => showList.value ? true : i <= 6 //tagLookup[tag]
           // ).map((tag) => <CheckboxTag tags={props.tags} tag={tag} />)}
-          checked.map((tag) => <CheckboxTag tags={props.tags} tag={tag} />)}
-          {unchecked.length > 0 && unchecked.filter((_, i) => showList.value ? true : i + checked.length < 6).map((tag) => <CheckboxTag tags={props.tags} tag={tag} />)}
+          checked.map((tag) => <CheckboxTag toggleTag={toggleTag} tags={props.tags} tag={tag} />)}
+        {unchecked.length > 0 && unchecked.filter((_, i) =>
+          showList.value ? true : i + checked.length < 6
+        ).map((tag) => <CheckboxTag toggleTag={toggleTag} tags={props.tags} tag={tag} />)}
       </ul>
     </td>
   );
@@ -95,71 +114,93 @@ const TagCategory = function TagCategory(
 const TagCategoryHeader = function TagCategoryHeader(
   props: { categoryName: string },
 ) {
-  const showList = useSignal(true);
-  const value = useSignal("");
+  const lowercaseCategoryNames = useComputed(() => {
+    return parsedTags.value[props.categoryName].map((tag) => tag.toLowerCase());
+  });
   return (
-    <th
-      class='sticky top-0'
-      // onMouseEnter={() => showList.value = true}
-      // onMouseLeave={() => showList.value = false}
-    >
+    <th class="sticky top-0">
       <p>{props.categoryName}</p>
-
-      {showList.value && (
-        <div class="flex items-center">
-          <input
-            type="text"
-            class=" text-indigo-600 transition duration-150 ease-in-out"
-            onChange={(e) => value.value = e.currentTarget?.value}
-          />
-          <button
-            class="ml-2 block text-sm leading-5 text-gray-900"
-            onClick={() => {
-              if (!parsedTags.value) return;
-              console.log(value.value);
-              // parsedTags.value[props.categoryName].push(value.value);
-              parsedTags.value[props.categoryName] = [ ...parsedTags.value[props.categoryName], value.value];
-              parsedFile.value = {...parsedFile.value};
-              value.value = "";
-            }}
-          >
-            +
-          </button>
-        </div>
-      )}
+      <InputBox
+        isValid={(value) => {
+          if (!parsedTags.value) {
+            return [false, "No file loaded"];
+          }
+          if (lowercaseCategoryNames.value.indexOf(value.toLowerCase()) > -1) {
+            return [false, "Item already exists"];
+          }
+          return [true, ""];
+        }}
+        onClick={(value) => {
+          if (!parsedTags.value) return false;
+          parsedTags.value[props.categoryName] = [
+            ...parsedTags.value[props.categoryName],
+            value,
+          ];
+          parsedFile.value = { ...parsedFile.value };
+          return true;
+        }}
+      />
     </th>
   );
 };
 
-const TagItemHeader = function TagItemHeader() {
-  const showList = useSignal(true);
+const InputBox = function InputBox(
+  props: {
+    isValid: (value: string) => [boolean, string];
+    onClick: (value: string) => boolean;
+  },
+) {
   const value = useSignal("");
+  const error = useSignal("");
+  const isValid = useSignal(true);
   return (
-    <th
-      class='sticky top-0'
-      // onMouseEnter={() => showList.value = true}
-      // onMouseLeave={() => showList.value = false}
-    >
-      <p>Item</p>
-      {showList.value && (
-        <div class="flex items-center">
-          <input
-            type="text"
-            class=" text-indigo-600 transition duration-150 ease-in-out"
-            onChange={(e) => value.value = e.currentTarget?.value}
-          />
-          <button
-            class="ml-2 block text-sm leading-5 text-gray-900"
-            onClick={() => {
-              if (!parsedFile.value) return;
-              parsedFile.value = { ...parsedFile.value, [value.value]: { Tags: [] }};
-              value.value = "";
-            }}
-          >
-            +
-          </button>
-        </div>
-      )}
+    <div>
+      <input
+        type="text"
+        class="w-3/4 border-1 text-indigo-600 transition duration-150 ease-in-out"
+        value={value.value}
+        onInput={(e) => {
+          const v = e.currentTarget?.value;
+          const [valIsValid, valError] = props.isValid(v);
+          error.value = valError || "";
+          isValid.value = valIsValid;
+          value.value = v;
+        }}
+      />
+      <button
+        class="w-1/4 text-sm text-gray-900"
+        onClick={() => {
+          props.onClick(value.value.trim()) && (value.value = "");
+        }}
+        disabled={!isValid.value || !value.value.trim()}
+      >
+        +
+      </button>
+      <div class="flex flex-row text-sm text-red-400">{error.value}</div>
+    </div>
+  );
+};
+
+const TagItemHeader = function TagItemHeader() {
+  return (
+    <th class="sticky top-0">
+      <p class="flex flex-row">Item</p>
+      <InputBox
+        isValid={(value) => {
+          if (!parsedFile.value) {
+            return [false, "No file loaded"];
+          }
+          if (parsedFile.value[value]) {
+            return [false, "Item already exists"];
+          }
+          return [true, ""];
+        }}
+        onClick={(value) => {
+          if (!parsedFile.value) return false;
+          parsedFile.value = { ...parsedFile.value, [value]: { Tags: [] } };
+          return true;
+        }}
+      />
     </th>
   );
 };
@@ -167,28 +208,56 @@ const TagItemHeader = function TagItemHeader() {
 const Entry = function Entry(
   props: { name: string; s: Signal<Tags>; f: Signal<File> },
 ) {
-  return (
-    <tr class='hover:bg-gray-100 align-top '>
-      <td class='sticky left-0 '>{props.name}</td>
-      {props.s.value &&
-        Object.keys(props.s.value).map((category) => (
-          <TagCategory
-            tags={props.f.value[props.name].Tags}
-            category={props.s.value[category]}
-            categoryName={category}
-          />
-        ))}
-    </tr>
-  );
+  const tags = useSignal(props.f.value[props.name].Tags);
+
+  const rest = props.s.value &&
+      Object.keys(props.s.value).map((category) => (
+        <TagCategory
+          tags={tags}
+          category={props.s.value[category]}
+          categoryName={category}
+        />
+      ));
+
+  const onLeave = () => {
+    if (_.isEqual(tags.value, props.f.value[props.name].Tags)) {
+      console.log('tags are not different, bye');
+      return;
+    }
+    props.f.value[props.name].Tags = tags.value;
+    props.f.value = { ...props.f.value };
+    console.log('tags are different, updating');
+  }
+
+  return  config.value.item_in_separate_row ? (
+    <><tr class="bg-white align-top ">
+        <td colSpan={Object.keys(props.s.value).length}>
+        {props.name}
+        </td>
+      </tr>
+    <tr class="hover:bg-gray-300 align-top " onMouseLeave={onLeave}>
+    <td></td>
+    { rest }
+  </tr>
+  </>
+  ) : (
+    <tr class="hover:bg-gray-300 align-top " onMouseLeave={onLeave}>
+    <td class="sticky left-0 bg-white">{props.name}</td>
+    { rest }
+  </tr>
+  )
+
+  ;
 };
 
 const List = function List() {
   if (!parsedFile.value) return null;
   if (!parsedTags.value) return null;
+  console.log('rendering list');
   return (
-    <table class='align-top'>
+    <table class="align-top">
       <thead>
-        <tr>
+        <tr class='sticky top-0 bg-white opacity-80 z-10'>
           <TagItemHeader />
           {Object.keys(parsedTags.value).map((category) => (
             <TagCategoryHeader categoryName={category} />
@@ -211,9 +280,7 @@ export default function Main() {
 
   return (
     <div class="container w-full mx-0 px-0">
-      <div class="p-8 mt-6 lg:mt-0 bg-white">
-        <List />
-      </div>
+      <List />
       <div class="grid grid-flow-col auto-cols-max grid-cols-6">
         <div>
           <h3>Yaml Tags</h3>
@@ -291,6 +358,17 @@ export default function Main() {
         >
           Get Yaml
         </button>
+        <input
+        type="checkbox"
+        class="h-4 w-4 text-indigo-600 transition duration-150 ease-in-out"
+        checked={config.value.item_in_separate_row}
+        onChange={() => {
+          config.value = { ...config.value, item_in_separate_row: !config.value.item_in_separate_row };
+        }}
+      />
+      <label class="ml-2 block text-sm leading-5 text-gray-900">
+        Item in separate row
+      </label>
       </div>
     </div>
   );
