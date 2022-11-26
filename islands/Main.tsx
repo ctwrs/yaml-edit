ac48ad0337f521816168daa7e610087a6d293efa
ctwrs <>
2022-11-26 21:08:33 +0100

    sticky headers, base for variable column width

--- islands/Main.tsx
+++ islands/Main.tsx
@@ -2,7 +2,13 @@ import { lodash as _ } from "lodash";
 import { parse, stringify } from "yaml";
 
 import { useEffect, useMemo } from "preact/hooks";
-import { Signal, signal, useComputed, useSignal } from "@preact/signals";
+import {
+  computed,
+  Signal,
+  signal,
+  useComputed,
+  useSignal,
+} from "@preact/signals";
 
 import { yamlFile, yamlTags } from "../data/TestData.ts";
 
@@ -17,12 +23,21 @@ if (typeof localStorage !== "undefined") {
   };
 }
 
-export const error = signal<{ mark: { buffer: string, position: number, line: number, column: number }; name: string; file: string; } | null>(null);
+const useUuid = () => useMemo(() => crypto.randomUUID(), []);
 
-type Config = {
-  item_in_separate_row: boolean;
-};
-const config = signal<Config>({ item_in_separate_row: false });
+export const error = signal<
+  {
+    mark: { buffer: string; position: number; line: number; column: number };
+    name: string;
+    file: string;
+  } | null
+>(null);
+
+type Config = Record<string, string>;
+const config = signal<Config>({
+  item_in_separate_row: "false",
+  enlarge_categories: "1",
+});
 
 type Tags = Record<string, string[]>;
 const parsedTags = signal<Tags>({});
@@ -39,6 +54,26 @@ const parseTags = (tags: string) => {
   }
   return parsed;
 };
+
+const widestTagCategories = computed(() => {
+  const tags = parsedTags.value;
+  console.log({ tags });
+  const categories = Object.keys(tags);
+  const widest = categories.reduce((acc, category) => {
+    console.log(tags[category]);
+    acc[category] = tags[category].sort((a, b) =>
+      a.length - b.length
+    ).reverse()[0];
+    console.log(acc[category]);
+    return acc;
+  }, {} as Record<string, string>);
+
+  const r = Object.keys(widest).sort((a, b) =>
+    widest[a].length - widest[b].length
+  ).reverse().slice(0, parseInt(config.value.enlarge_categories)); // an array of categories sorted by length of the widest tag
+  console.log({ tags, r });
+  return r;
+});
 
 type File = Record<string, { Tags: string[] }>;
 const parsedFile = signal<File>({});
@@ -58,9 +93,13 @@ const parseFile = (file: string) => {
 };
 
 const CheckboxTag = function CheckboxTag(
-  props: { toggleTag: (tag: string) => void; tags: Signal<string[]>; tag: string },
+  props: {
+    toggleTag: (tag: string) => void;
+    tags: Signal<string[]>;
+    tag: string;
+  },
 ) {
-  const uuid = useMemo(() => crypto.randomUUID(), []);
+  const uuid = useUuid();
   const tagIndex = props.tags.value.indexOf(props.tag);
   return (
     <div class="flex items-center">
@@ -87,9 +126,14 @@ const partition = (arr: string[], predicate: (item: string) => boolean) => {
 };
 
 const TagCategory = function TagCategory(
-  props: { tags: Signal<string[]>; category: string[]; categoryName: string },
+  props: {
+    tags: Signal<string[]>;
+    category: string[];
+    categoryName: string;
+    colSpan: number;
+  },
 ) {
-  const showList = useSignal(false);
+  const showList = useSignal(true);
 
   const [checked, unchecked] = useMemo(
     () => partition(props.category, (tag) => props.tags.value.includes(tag)),
@@ -104,22 +148,21 @@ const TagCategory = function TagCategory(
   };
   return (
     <td
-      class="text-xs px-1 text-gray-300"
-      onMouseEnter={() => showList.value = true}
-      onMouseLeave={() => {
-        showList.value = false
-
-      }}
+      class="text-sm px-1 text-gray-300"
+      colSpan={props.colSpan}
     >
-      <ul>
-        {props.category &&
-          // props.category.filter((tag, i) => showList.value ? true : i <= 6 //tagLookup[tag]
-          // ).map((tag) => <CheckboxTag tags={props.tags} tag={tag} />)}
-          checked.map((tag) => <CheckboxTag toggleTag={toggleTag} tags={props.tags} tag={tag} />)}
-        {unchecked.length > 0 && unchecked.filter((_, i) =>
-          showList.value ? true : i + checked.length < 6
-        ).map((tag) => <CheckboxTag toggleTag={toggleTag} tags={props.tags} tag={tag} />)}
-      </ul>
+      <div class=" overflow-auto overflow-x-auto h-64">
+        <ul>
+          {props.category &&
+            checked.map((tag) => (
+              <CheckboxTag toggleTag={toggleTag} tags={props.tags} tag={tag} />
+            ))}
+          {unchecked.length > 0 &&
+            unchecked.map((tag) => (
+              <CheckboxTag toggleTag={toggleTag} tags={props.tags} tag={tag} />
+            ))}
+        </ul>
+      </div>
     </td>
   );
 };
@@ -130,30 +173,41 @@ const TagCategoryHeader = function TagCategoryHeader(
   const lowercaseCategoryNames = useComputed(() => {
     return parsedTags.value[props.categoryName].map((tag) => tag.toLowerCase());
   });
+  console.log(props.categoryName, widestTagCategories.value);
   return (
-    <th class="sticky top-0">
-      <p>{props.categoryName}</p>
-      <InputBox
-        isValid={(value) => {
-          if (!parsedTags.value) {
-            return [false, "No file loaded"];
-          }
-          if (lowercaseCategoryNames.value.indexOf(value.toLowerCase()) > -1) {
-            return [false, "Item already exists"];
-          }
-          return [true, ""];
-        }}
-        onClick={(value) => {
-          if (!parsedTags.value) return false;
-          parsedTags.value[props.categoryName] = [
-            ...parsedTags.value[props.categoryName],
-            value,
-          ];
-          parsedFile.value = { ...parsedFile.value };
-          return true;
-        }}
-      />
-    </th>
+    <>
+      <th
+        class="sticky top-0"
+        colSpan={widestTagCategories.value.includes(props.categoryName) ? 2 : 1}
+      >
+        <p>{props.categoryName}</p>
+        <InputBox
+          isValid={(value) => {
+            if (!parsedTags.value) {
+              return [false, "No file loaded"];
+            }
+            if (
+              lowercaseCategoryNames.value.indexOf(value.toLowerCase()) > -1
+            ) {
+              return [false, "Item already exists"];
+            }
+            return [true, ""];
+          }}
+          onClick={(value) => {
+            if (!parsedTags.value) return false;
+            parsedTags.value[props.categoryName] = [
+              ...parsedTags.value[props.categoryName],
+              value,
+            ];
+            parsedFile.value = { ...parsedFile.value };
+            return true;
+          }}
+        />
+      </th>
+      {widestTagCategories.value.includes(props.categoryName)
+        ? <th></th>
+        : <></>}
+    </>
   );
 };
 
@@ -196,7 +250,12 @@ const InputBox = function InputBox(
 
 const TagItemHeader = function TagItemHeader() {
   return (
-    <th class="sticky top-0">
+    <th
+      class="sticky top-0"
+      colSpan={config.value.item_in_separate_row
+        ? Object.keys(parsedTags.value).length + 1
+        : 1}
+    >
       <p class="flex flex-row">Item</p>
       <InputBox
         isValid={(value) => {
@@ -224,58 +283,77 @@ const Entry = function Entry(
   const tags = useSignal(props.f.value[props.name].Tags);
 
   const rest = props.s.value &&
-      Object.keys(props.s.value).map((category) => (
-        <TagCategory
-          tags={tags}
-          category={props.s.value[category]}
-          categoryName={category}
-        />
-      ));
+    Object.keys(props.s.value).map((category) => (
+      <TagCategory
+        tags={tags}
+        category={props.s.value[category]}
+        categoryName={category}
+        colSpan={widestTagCategories.value.includes(category) ? 2 : 1}
+      />
+    ));
 
   const onLeave = () => {
     if (_.isEqual(tags.value, props.f.value[props.name].Tags)) {
-      console.log('tags are not different, bye');
+      console.log("tags are not different, bye");
       return;
     }
     props.f.value[props.name].Tags = tags.value;
     props.f.value = { ...props.f.value };
-    console.log('tags are different, updating');
-  }
+    console.log("tags are different, updating");
+  };
 
-  return  config.value.item_in_separate_row ? (
-    <><tr class="bg-white align-top ">
-        <td colSpan={Object.keys(props.s.value).length + 1}>
-        {props.name}
-        </td>
+  return config.value.item_in_separate_row
+    ? (
+      <>
+        <tr class="bg-white align-top ">
+          <td colSpan={Object.keys(props.s.value).length + 1}>
+            {props.name}
+          </td>
+        </tr>
+        <tr class="hover:bg-gray-300 align-top " onMouseLeave={onLeave}>
+          <td></td>
+          {rest}
+        </tr>
+      </>
+    )
+    : (
+      <tr class="hover:bg-gray-300 align-top " onMouseLeave={onLeave}>
+        <td class="sticky left-0 bg-white">{props.name}</td>
+        {rest}
       </tr>
-    <tr class="hover:bg-gray-300 align-top " onMouseLeave={onLeave}>
-    <td></td>
-    { rest }
-  </tr>
-  </>
-  ) : (
-    <tr class="hover:bg-gray-300 align-top " onMouseLeave={onLeave}>
-    <td class="sticky left-0 bg-white">{props.name}</td>
-    { rest }
-  </tr>
-  )
-
-  ;
+    );
 };
 
 const List = function List() {
   if (!parsedFile.value) return null;
   if (!parsedTags.value) return null;
-  console.log('rendering list');
+  console.log("rendering list");
+
   return (
-    <table class="align-top">
+    <table class="align-top table-fixed">
       <thead>
-        <tr class='sticky top-0 bg-white opacity-80 z-10'>
-          <TagItemHeader />
-          {Object.keys(parsedTags.value).map((category) => (
-            <TagCategoryHeader categoryName={category} />
-          ))}
-        </tr>
+        {config.value.item_in_separate_row
+          ? (
+            <>
+              <tr class="sticky top-0 bg-white opacity-80 z-10">
+                <TagItemHeader />
+              </tr>
+              <tr class="sticky top-12 mt-5 bg-white opacity-80 z-10">
+                <td></td>
+                {Object.keys(parsedTags.value).map((category) => (
+                  <TagCategoryHeader categoryName={category} />
+                ))}
+              </tr>
+            </>
+          )
+          : (
+            <tr class="sticky top-0 bg-white opacity-80 z-10">
+              <TagItemHeader />
+              {Object.keys(parsedTags.value).map((category) => (
+                <TagCategoryHeader categoryName={category} />
+              ))}
+            </tr>
+          )}
       </thead>
       <tbody>
         {Object.keys(parsedFile.value).map((key) => (
@@ -288,27 +366,75 @@ const List = function List() {
 };
 
 const dl = (value: string, fileName: string) => {
-
   const tempLink = document.createElement("a");
-  const taBlob = new Blob([value], {type: 'text/plain'});
+  const taBlob = new Blob([value], { type: "text/plain" });
 
-  tempLink.setAttribute('href', URL.createObjectURL(taBlob));
-  tempLink.setAttribute('download', `${fileName.toLowerCase()}`);
+  tempLink.setAttribute("href", URL.createObjectURL(taBlob));
+  tempLink.setAttribute("download", `${fileName.toLowerCase()}`);
   tempLink.click();
 
   URL.revokeObjectURL(tempLink.href);
 };
 
+const Option = function Option(
+  props: { key: string; description: string; options?: string[] },
+) {
+  const uuid = useUuid();
+  return (props.options?.length
+    ? (
+      <>
+        <select
+          id={uuid}
+          onChange={(e) => {
+            if (!config.value[props.key]) return;
+            config.value[props.key] = e.currentTarget.value;
+            config.value = { ...config.value };
+          }}
+          value={config.value[props.key]}
+        >
+          {props.options.map((option) => (
+            <option value={option}>{option}</option>
+          ))}
+        </select>
+        <label for={uuid} class="ml-2 block text-sm leading-5 text-gray-900">
+          {props.description}
+        </label>
+      </>
+    )
+    : (
+      <>
+        <input
+          type="checkbox"
+          id={uuid}
+          class="h-4 w-4 text-indigo-600 transition duration-150 ease-in-out"
+          checked={config.value[props.key] === "true"}
+          onChange={() => {
+            config.value = {
+              ...config.value,
+              [props.key]: config.value[props.key] === "false"
+                ? "true"
+                : "false",
+            };
+            ls.setItem("config", JSON.stringify(config.value));
+          }}
+        />
+        <label for={uuid} class="ml-2 block text-sm leading-5 text-gray-900">
+          {props.description}
+        </label>
+      </>
+    ));
+};
+
 export default function Main() {
-  const localYamlTags = useSignal('');
-  const localYamlFile = useSignal('');
+  const localYamlTags = useSignal("");
+  const localYamlFile = useSignal("");
 
   const triedLoading = useSignal(false);
 
   useEffect(() => {
     if (triedLoading.value) return;
     triedLoading.value = true;
-    localYamlTags.value = ls.getItem('yamlTags') || yamlTags;
+    localYamlTags.value = ls.getItem("yamlTags") || yamlTags;
     const tags = parseTags(localYamlTags.value);
     if (!tags) {
       console.error("no tags");
@@ -316,7 +442,7 @@ export default function Main() {
     }
     parsedTags.value = tags;
 
-    localYamlFile.value = ls.getItem('yamlFile') || yamlFile;
+    localYamlFile.value = ls.getItem("yamlFile") || yamlFile;
     const file = parseFile(localYamlFile.value);
     if (!file) {
       console.error("no file");
@@ -324,19 +450,21 @@ export default function Main() {
     }
     parsedFile.value = file;
 
-    config.value = JSON.parse(ls.getItem('config') || JSON.stringify(config.value));
+    config.value = JSON.parse(
+      ls.getItem("config") || JSON.stringify(config.value),
+    );
   }, [triedLoading.value]);
 
   useEffect(() => {
     const i = setInterval(() => {
       if (Object.keys(parsedTags.value).length > 0) {
         const tags = stringify(parsedTags.value);
-        ls.setItem('yamlTags', tags);
+        ls.setItem("yamlTags", tags);
         localYamlTags.value = tags;
       }
       if (Object.keys(parsedFile.value).length > 0) {
         const file = stringify(parsedFile.value);
-        ls.setItem('yamlFile', file);
+        ls.setItem("yamlFile", file);
         localYamlFile.value = file;
       }
     }, 10000);
@@ -345,21 +473,25 @@ export default function Main() {
   }, []);
 
   return (
-
     <div class="container w-full mx-0 px-0">
       <List />
       {error.value && (
-          <div
-            class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
-            role="alert"
-          >
-            <strong class="font-bold">Error in yaml {error.value.file}!</strong>
-            <span class="block sm:inline">
-              <div class="p-4">{error.value.mark.buffer.substring(error.value.mark.position - 30, error.value.mark.position + 30).split('\n').map(x => <p>{x}</p>)}</div>
-              {`line: ${error.value.mark.line}, column: ${error.value.mark.column}`}
-            </span>
-          </div>
-        )}
+        <div
+          class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+          role="alert"
+        >
+          <strong class="font-bold">Error in yaml {error.value.file}!</strong>
+          <span class="block sm:inline">
+            <div class="p-4">
+              {error.value.mark.buffer.substring(
+                error.value.mark.position - 30,
+                error.value.mark.position + 30,
+              ).split("\n").map((x) => <p>{x}</p>)}
+            </div>
+            {`line: ${error.value.mark.line}, column: ${error.value.mark.column}`}
+          </span>
+        </div>
+      )}
       <div class="grid grid-flow-col auto-cols-max grid-cols-6">
         <div>
           <h3>Yaml Tags</h3>
@@ -407,10 +539,10 @@ export default function Main() {
           onClick={() => {
             const date = new Date()
               .toISOString()
-              .split('')
+              .split("")
               .filter((l) => /[0-9]/g.test(l))
               .slice(0, 14)
-              .join('');
+              .join("");
             dl(stringify(parsedTags.value), `${date}-tags.yaml`);
             dl(stringify(parsedFile.value), `${date}-file.yaml`);
           }}
@@ -432,18 +564,15 @@ export default function Main() {
         >
           Save yaml to clipboard
         </button>
-        <input
-        type="checkbox"
-        class="h-4 w-4 text-indigo-600 transition duration-150 ease-in-out"
-        checked={config.value.item_in_separate_row}
-        onChange={() => {
-          config.value = { ...config.value, item_in_separate_row: !config.value.item_in_separate_row };
-          ls.setItem('config', JSON.stringify(config.value));
-        }}
-      />
-      <label class="ml-2 block text-sm leading-5 text-gray-900">
-        Item label in separate row
-      </label>
+        <Option
+          key="item_in_separate_row"
+          description="Item label in separate row?"
+        />
+        <Option
+          key="enlarge_categories"
+          description="How many categories should have double width?"
+          options={["0", "1", "2", "3", "4", "5", "6"]}
+        />
       </div>
     </div>
   );
